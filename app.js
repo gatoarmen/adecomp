dcb409ccebce551500af099000f7b7219cf6dccf
esteban <esteban_armendariz@hotmail.com>
2018-09-28 19:40:55 -0500

    working enviroment done

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ var PORT = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
